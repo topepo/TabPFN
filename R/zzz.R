9c39ee17cb9aa00beff77a0232ac1d2a0608b466
‘topepo’ <mxkuhn@gmail.com>
2025-03-13 07:52:15 -0400

    changes for #3

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,20 @@
-
 tabpfn <- NULL
 
 .onLoad <- function(...) {
+  pkg <- asNamespace("TabPFN")
+  pkg$check_libomp()
 
   reticulate::py_require("tabpfn")
 
   tryCatch(
-    tabpfn <<- reticulate::import("tabpfn", delay_load = list(
-      on_error = function(e) {
-        cli::cli_abort(msg_tabpfn_not_available(e))
-      }
-    )),
+    tabpfn <<- reticulate::import(
+      "tabpfn",
+      delay_load = list(
+        on_error = function(e) {
+          cli::cli_abort(msg_tabpfn_not_available(e))
+        }
+      )
+    ),
 
     # if reticulate has already loaded symbols from a Python installation,
     # `reticulate::import(delay_load = TRUE)` will error immediately.
