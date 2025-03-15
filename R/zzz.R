661e7a8f05dad6b5588a0d3e826a791c3018b2d0
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-03-15 12:02:54 -0400

    `patch_libllvmlite()` on mac

--- R/zzz.R
+++ R/zzz.R
@@ -19,6 +19,11 @@ autotabpfn <- NULL
     tabpfn <<- reticulate::import(
       "tabpfn",
       delay_load = list(
+        before_load = function() {
+          if (is_macOS()) {
+            patch_libllvmlite()
+          }
+        },
         on_error = function(e) {
          msg <- not_available_msg("tabpfn")
          cli::cli_abort(msg)
@@ -38,6 +43,11 @@ autotabpfn <- NULL
   autotabpfn <<- reticulate::import(
    "tabpfn_extensions.post_hoc_ensembles.sklearn_interface",
    delay_load = list(
+     before_load = function() {
+       # force the 'tabpfn' lazy module to load and run the patch in the
+       # before_load() hook
+       tabpfn$`__version__`
+     },
     on_error = function(e) {
      msg <- not_available_msg("tabpfn_extensions.post_hoc_ensembles.sklearn_interface")
      cli::cli_abort(msg)
@@ -50,4 +60,21 @@ autotabpfn <- NULL
    cli::cli_abort(msg)
   }
  )
+}
+
+
+patch_libllvmlite <- function(venv_prefix = reticulate::import("sys")$prefix) {
+  libllvmlite.dylib <- unique(normalizePath(list.files(
+    venv_prefix,
+    pattern = "libllvmlite\\.dylib$",
+    recursive = TRUE,
+    full.names = TRUE
+  )))
+  for (lib in libllvmlite.dylib)
+    system2("install_name_tool", c("-add_rpath /usr/lib", shQuote(lib)))
+}
+
+
+is_macOS <- function() {
+  Sys.info()[["sysname"]] == "Darwin"
 }
